794c07e2f61e5bcf3bc8fa707b64468be521f358
Mohamed Khalifa <7mdiano@gmail.com>
2023-04-25 23:19:41 +0200

    new model

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 import uvicorn
-from model.model import RecommendationSystem
+# from old_model.model import RecommendationSystem
+from model.main import RecommendationSystem
 import numpy as np
 import json
 # MongoDB
@@ -19,13 +20,89 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.applications.vgg16 import preprocess_input
 
+import datetime
+import os
 
-products = np.array(json.load(open('data.json', 'rb')))
+
+products = np.array(json.load(open('product_thumbnail.json', 'rb')))
 formated_data = []
 product_id = []
 for p in products:
     formated_data.append(p["formated_img"])
     product_id.append(p["_id"])
+
+def pipeline(res):
+    return [
+    {'$match' : { '_id': { '$in': res } }},
+    {
+        '$lookup': {
+            'from': 'colors', 
+            'localField': '_id', 
+            'foreignField': 'product', 
+            'as': 'color'
+        }
+    }, {
+        '$lookup': {
+            'from': 'sizes', 
+            'localField': 'color._id', 
+            'foreignField': 'color', 
+            'as': 'size'
+        }
+    }, {
+        '$lookup': {
+            'from': 'reviews', 
+            'localField': 'reviews', 
+            'foreignField': '_id', 
+            'as': 'reviews'
+        }
+    }, {
+        '$project': {
+            '_id': 1, 
+            'seller': 1, 
+            'title': 1, 
+            'category': 1, 
+            'subcategory': 1, 
+            'color': '$color.color', 
+            'size': '$size.name', 
+            'images': {
+                '$first': '$color.images'
+            }, 
+            'price': {
+                '$toDouble': {
+                    '$first': '$color.price'
+                }
+            }, 
+            'discount': {
+                '$first': '$color.discount'
+            }, 
+            'n': {
+                '$size': '$reviews'
+            }, 
+            'rating': {
+                '$cond': {
+                    'if': {
+                        '$eq': [
+                            {
+                                '$size': '$reviews'
+                            }, 0
+                        ]
+                    }, 
+                    'then': 0, 
+                    'else': {
+                        '$divide': [
+                            {
+                                '$sum': '$reviews.rating'
+                            }, {
+                                '$size': '$reviews'
+                            }
+                        ]
+                    }
+                }
+            }, 
+            'createdAt': 1
+        }
+    }
+]
 
 app = FastAPI()
 
@@ -65,8 +142,22 @@ async def recommend(file:bytes = File(...), n:int = 5):
     extension = imghdr.what(None, file)
     print(extension)
     img = Image.open(BytesIO(file))
+    print(file)
+    # Make class instance
+    rec = RecommendationSystem(formated_data, product_id)
+    # # Recommend n images based on the image provided
+    res = rec.recommender(img, n)
+    
+    documents = client.makask.products.find({"_id": {"$in": res}})
+    
+    return list(documents)
+
+@app.post("/recommend/v2")
+async def recommend(file:bytes = File(...), n:int = 5):
+    extension = imghdr.what(None, file)
+    img = Image.open(BytesIO(file))
     # save the image in its original format
-    filename = 'temp.' + extension
+    filename = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+'.' + extension
     img.save(filename)
     # image preprocessing
 
@@ -75,9 +166,14 @@ async def recommend(file:bytes = File(...), n:int = 5):
     # # Recommend n images based on the image provided
     res = rec.recommender(filename, n)
     
-    documents = client.makask.products.find({"_id": {"$in": res}})
-    
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    # documents = client.makask.products.find({"_id": {"$in": res}})
+    documents = client.makask.products.aggregate(pipeline(res))
+
     return list(documents)
+
 # uvicorn main:app --reload
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
